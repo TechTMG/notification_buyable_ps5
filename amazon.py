886dbafd0f4ec724d389c2306ec535a2bba25e25
KosukeShimizu <thimi0412@icloud.com>
2021-02-28 13:46:28 +0900

    amazonの処理を関数化

--- amazon.py
+++ amazon.py
@@ -7,7 +7,12 @@ AMAZON = "https://www.amazon.co.jp/dp/B08GGF7M7B/?th=1"
 # AMAZON = "https://www.amazon.co.jp/dp/B08GGGBKRQ/?th=1"
 
 
-def is_buyable():
+def is_amazon_buyable():
+    """アマゾンに商品があるかcheckする
+
+    Returns:
+        [str]: 商品があれば'カートに入れる'、なければ空文字
+    """
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -17,12 +22,14 @@ def is_buyable():
     driver.get(AMAZON)
     # デジタルエディション版のボタンをクリック
     driver.find_element_by_id("a-autoid-9-announce").click()
+
     sleep(1)
-    a = driver.find_element_by_class_name('a-button-input').get_attribute("value")
-    print(a)
+    is_buyable = driver.find_element_by_class_name('a-button-input').get_attribute("value")
 
     driver.close()
     driver.quit()
 
+    return is_buyable
+
 if __name__ == '__main__':
-    is_buyable()
+    is_amazon_buyable()
